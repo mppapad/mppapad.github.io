29bf56a9471c86c490cdd23561f56b4cd41a15bd
Miltiades papadopoulos <38037217+mppapad@users.noreply.github.com>
2023-03-25 16:08:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+// Hamburger menu toggle scrpit:
+toggleButton.addEventListener('click',() => {
+    navbarLinks.classList.toggle('active')
+})
+
+// Theme switch script:
+var icon = document.getElementById("themeIcon");
+icon.onclick = function() {
+    document.body.classList.toggle("white-theme");
+    if( document.body.classList.contains("white-theme")) {
+        themeIcon.src = "images/moon.png";
+        logosvg.src = "images/text324.svg";
+    }
+    else {
+        themeIcon.src = "images/sun.png";
+        logosvg.src = "images/text325.svg";
+    }
+}
