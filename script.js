6617b1e8b1082ea28d365380ab4c65647edfa995
Miltiadis papadopoulos <iis21063@uom.edu.gr>
2023-06-01 14:38:52 +0300

    Navbar Layout fix
    - Added new dark mode switch
    - Fixed switch layout issues in responsive mode
    - New title
    - Added new Favicon
    - HTML cleanup

--- script.js
+++ script.js
@@ -7,15 +7,26 @@ toggleButton.addEventListener('click',() => {
 })
 
 // Theme switch script:
-var icon = document.getElementById("themeIcon");
+var icon = document.getElementById("switch");
 icon.onclick = function() {
     document.body.classList.toggle("white-theme");
     if( document.body.classList.contains("white-theme")) {
-        themeIcon.src = "images/moon.png";
+        /*themeIcon.src = "images/moon.png";*/
         logosvg.src = "images/MppapadLogo-dark.svg";
     }
     else {
-        themeIcon.src = "images/sun.png";
+        /*themeIcon.src = "images/sun.png";*/
         logosvg.src = "images/MppapadLogo-Light.svg";
     }
+}
+
+//Preloader vanish script
+var loader = document.querySelector(".loader-wrapper")
+var loaderDiv = document.getElementById("loaderdiv");
+
+window.addEventListener("load", vanish);
+
+function vanish(){
+    loader.classList.add("loader-hidden");
+    //loaderDiv.remove();
 }
